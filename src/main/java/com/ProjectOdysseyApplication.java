779ac9639487aa000b9cbc56dfa2292cd5c30bbb
shayeong <tlsgkdud1999@gmail.com>
2024-01-18 16:36:15 +0900

    project_obyssey

---
+++ src/main/java/com/ProjectOdysseyApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOdysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOdysseyApplication.class, args);
+	}
+
+}
